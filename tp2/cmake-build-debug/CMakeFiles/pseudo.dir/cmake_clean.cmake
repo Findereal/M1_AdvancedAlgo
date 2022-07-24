2c13683825cce07065807c1018f2a57eba04a3f3
Findereal <75438148+Findereal@users.noreply.github.com>
2022-07-24 16:08:19 +0200

    contains tp2 files

---
+++ tp2/cmake-build-debug/CMakeFiles/pseudo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pseudo.dir/pseudo.c.o"
+  "pseudo"
+  "pseudo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pseudo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
